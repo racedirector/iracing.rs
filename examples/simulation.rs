a9d7465e48e11f4f22777515e2703434cf342585
Justin Makaila <1638996+justinmakaila@users.noreply.github.com>
2025-11-20 18:10:03 -0500

    update simulation example

--- examples/simulation.rs
+++ examples/simulation.rs
@@ -1,24 +1,24 @@
 use iracing::simulation::Simulation;
+use std::{thread::sleep, time::Duration};
 
 pub fn main() {
     let simulation = Simulation {
         host: String::from("127.0.0.1"),
     };
 
-    println!("Waiting for iRacing simulation connection...");
+    loop {
+        println!("Waiting for iRacing simulation connection...");
+        // Wait for a connection
+        while !simulation.is_connected() {
+            sleep(Duration::from_secs(1))
+        }
 
-    // Check for a sim connection every second
-    while !simulation.is_connected() {
-        std::thread::sleep(std::time::Duration::from_secs(1))
+        println!("iRacing connected!");
+
+        while simulation.is_connected() {
+            sleep(Duration::from_millis(500))
+        }
+
+        println!("iRacing disconnected!");
     }
-
-    println!("iRacing connected!");
-
-    // Check for sim disconnection every second
-    while simulation.is_connected() {
-        // !!!: Here is where you'd do simulation related work; start your connection, read session info, etc.
-        std::thread::sleep(std::time::Duration::from_secs(1))
-    }
-
-    println!("iRacing disconnected!")
 }
