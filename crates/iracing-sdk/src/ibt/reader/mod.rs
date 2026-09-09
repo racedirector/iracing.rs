eddc57303ebfd4c5f2192ae5129f08c7a468c99e
Justin Makaila <1638996+justinmakaila@users.noreply.github.com>
2026-09-09 09:26:36 -0400

    use ibt validation

--- crates/iracing-sdk/src/ibt/reader/mod.rs
+++ crates/iracing-sdk/src/ibt/reader/mod.rs
@@ -85,7 +85,7 @@ impl IbtReader {
 
         // Parse IBT header
         let header = Header::try_from_reader(&mut cursor)?;
-        header.validate()?;
+        header.validate_ibt()?;
 
         // Parse disk sub-header (note: may be corrupted, but we'll try)
         let disk_header = DiskSubHeader::try_from_reader(&mut cursor)?;
