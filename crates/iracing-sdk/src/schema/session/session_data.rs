d75a00d1b2e223137acf148346d58734854e12bd
Justin Makaila <1638996+justinmakaila@users.noreply.github.com>
2026-09-15 11:52:51 -0400

    Update serialization

--- crates/iracing-sdk/src/schema/session/session_data.rs
+++ crates/iracing-sdk/src/schema/session/session_data.rs
@@ -66,7 +66,6 @@ pub struct Session {
     #[cfg_attr(feature = "codegen", schemars(with = "Option<Vec<serde_json::Value>>"))]
     pub results_fastest_lap: Option<Vec<serde_yaml_ng::Value>>,
     /// Results qualifying
-    #[cfg_attr(feature = "codegen", schemars(with = "Option<Vec<serde_json::Value>>"))]
     pub qualify_positions: Option<Vec<QualifyResult>>,
     /// Results average lap time
     pub results_average_lap_time: Option<f64>,
